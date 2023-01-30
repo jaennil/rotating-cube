0493b97501f39335c7302992d3eaa0c3f8d56892
johnil <98917669+ssushnost@users.noreply.github.com>
2023-01-31 01:20:40 +0300

    learning stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let rows = 5;
+    let columns = 3;
+    let mut field = vec![vec![0; columns]; rows];
+    print(&field);
+}
+
+fn print(field: &Vec<Vec<i32>>) {
+    for row in field {
+        for cell in row {
+            print!("{} ", cell);
+        }
+        println!();
+    }
 }
